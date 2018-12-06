2235e9520ab8c506c9aa2a0e1ae22fb6029c351f
blackcat <stef4oben@abv.bg>
2018-12-06 12:52:58 +0200

    Bug fixes   ~Fixed comment string bug   *Restructured code   +Added exception when using plugin while in invalid view

--- CommentDeleter/comment_plugin.py
+++ CommentDeleter/comment_plugin.py
@@ -19,12 +19,19 @@ class Comment:
 		self.regex = self.regex_compiler(multi_start, multi_end, single)
 		self.types.append(self)
 
+Comment("\\/\\*", "\\*\\/", "\\/\\/", ["c", "cpp", "csharp", "rs", "hh", "cc"])
+Comment(None, None, "#", ["py"])
+Comment("=start", "=end", "#", ["rb"])
+Comment("<!--", "-->", None, ["html"])
+Comment("\\/\\*", "\\*\\/", None, ["css"])
 
 class CommentDeleterCommand(sublime_plugin.TextCommand):
 	
 			
 	def build_comment(self):
 		name = self.view.file_name()
+		if name is None:
+			raise Exception("Not in a view")
 		name = path.split(name)
 		ext = name[1].split(".")[1]
 		comments = [x for x in Comment.types if ext in x.ext]
@@ -36,16 +43,15 @@ class CommentDeleterCommand(sublime_plugin.TextCommand):
 	def run(self, edit, one_delete=False, from_cursor_pos=False, files=[]):
 		start_pos = self.view.sel()[0].begin() if from_cursor_pos else 0
 		removed=0
-		Comment("\\/\\*", "\\*\\/", "\\/\\/", ["c", "cpp", "csharp", "rs"])
-		Comment(None, None, "#", ["py"])
-		Comment("=start", "=end", "#", ["rb"])
-		Comment("<!--", "-->", None, ["html"])
-		Comment("\\/\\*", "\\*\\/", None, ["css"])
 		comment = self.build_comment()
 		while True:
 			region = self.view.find(comment.regex, start_pos)
 			if region.empty():
 				break
+			before_full = self.view.substr(sublime.Region(0, region.a))
+			if before_full.count('"') % 2 != 0 or before_full.count("'") % 2 != 0:
+				start_pos = region.b
+				continue
 			line = self.view.line(region.a)
 			part = sublime.Region(line.a, region.a)
 			part2 = sublime.Region(region.b, line.b)
@@ -66,5 +72,5 @@ class CommentDeleterCommand(sublime_plugin.TextCommand):
 			if one_delete or removed >= 600:
 				break
 			
-		print("Removed {} comment{}.".format(removed, "s" if removed > 1 else ""))
+		print("Removed {} comment{}.".format(removed, "s" if removed > 1 else ""))# za domashno da si razgledame coda po OOP s unarnite prostutii, zashtoto ne go razbirame i mi e sloshen
 			
